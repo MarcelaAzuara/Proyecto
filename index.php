0f58205eb5857e18471046f628d7f1c47e95e10e
MarcelaAzuara <114698799+MarcelaAzuara@users.noreply.github.com>
2022-09-29 20:38:43 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,427 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV Personal</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/310deaaacf.js" crossorigin="anonymous"></script>
+
+</head>
+<style >
+    {
+    box-sizing: border-box;
+}
+    body{
+    font-family: 'Balsamiq Sans', cursive;
+    background: url(imag/fondo.gif);
+    background-repeat:no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    min-height: 100vh;
+    align-items: center;
+    border: 5px solid white;
+}
+    
+</style>
+<body>
+	<!-------------------------------------------------------->
+	<div class="container">
+	<nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: #C1A5EE;">
+  	<div class="container-fluid">
+    <a class="navbar-brand" href="index2.html">CURRICULUM VITAE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Sobre mi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#estudios">Estudios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#portafolio">Portafolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#intereces">Intereces</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#Contacto">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br><br><br>
+<!-------------------------------------------------------->
+
+<div class="row" id="Sobre mi" style="background-color: #C1A5EE;">
+
+  <div class="col-6"  >
+    <center>
+      <h1 style="background-color:#C1A5EE; color: white;">Marcela Azuara Rivera</h1> <br><br>
+      <img width="60%" src="imag/foto1.jpg"><br><br>
+      <h3 style="background-color:#C1A5EE; color: white;">Curriculum Vitae</h3>
+    </center>
+  </div>
+  
+  <div class="col-6 col">
+    <center>
+    <h3 style="padding-top: 20%;"><p style="background-color:#C1A5EE; color: white;">«Si no puedes volar, corre; si no puedes correr, camina; si no puedes caminar, arrástrate, pero hagas lo que hagas sigue moviéndote hacia adelante» Martin Luther King Jr.</p></h3>
+    </center>
+  </div>
+
+</div>
+<br><br><br>
+<!-------------------------------------------------------->
+<div class="container">
+  <div id="estudios">
+    <h1 class="display-1" align="center" style="background-color:#C1A5EE; color: white;";>Estudios</h1>
+  <hr>
+		<div class="row">
+  <div class="col-6">
+    <div class="container">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imag/img1.jpg" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img2.jpg" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img4.jpg" class="d-block w-100" alt="">
+    </div>
+    </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+  </div>
+
+  <div class="col-6">
+    <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading1">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+        Secundaria
+      </button>
+    </h2>
+    <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1" data-bs-parent="#accordionExample">
+      <div class="accordion-body" style="text-align: justify;">
+         Estudie mi Secundaria en la Benemerito Instituto Normal del Estado "Juan Crisostomo Bonilla", estuve ahi 3 años seguidos preparandome para llegar al bachiller.
+         Hice unos cuentos amigos, lo normal.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading3">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">Nombre de la Institución educativa</button>
+    </h2>
+    <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#accordionExample">
+      <div class="accordion-body"style="text-align: justify;">
+        <strong> Benemerito Instituto Normal del Estado "Juan Crisostomo Bonilla"</strong>
+      </div>
+    </div>
+  </div>
+ <div class="accordion-item">
+    <h2 class="accordion-header" id="heading4">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">Bachillerato</button>
+    </h2>
+    <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#accordionExample">
+      <div class="accordion-body" style="text-align: justify;">
+        Ahora bien, en el bachiller hice mi examen el la misma escuela y volvi a cursar mes 3 años de bachiller en el Benemerito Instituto Normal del Estado "Juan Crisostomo Bonilla", ahi hice muchos más amigos, estuve en la feria academica de matematicas, conoci a Jose Aldair que actualmente es mi novio, llevamos 3 años juntos y me gradue, con un promedio regular.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading5">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">TSU</button>
+    </h2>
+    <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading5" data-bs-parent="#accordionExample">
+      <div class="accordion-body" style="text-align: justify;">Actualmente estudio para Técnica Superior Universitaria en el Área de Desarrollo de Software Multiplataformas en la Universidad Tecnológica de Huejotzingo.
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading5">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">Cursos</button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body" style="text-align: justify;">
+        <ul> 
+          <li>Plantzi: Curso de diseño visual de Marcas.</li>
+          <li>Platzi: Curso de Java SE orientado a objetos.</li>
+          <li>Platzi: Cursos De Programacion Web/Aprende programación de cero</li>
+          <li>Udemy: Curso de HTML5 y CSS3.</li>
+          <li>Udemy: CSS Básico + Introduccion a Flexbox</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Conocimientos y Habilidades</button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body" style="text-align: justify;">
+        <h4>Conocimientos:</h4>
+        <ol> 
+          <li>Digital: usar la tecnología de forma eficiente y segura.</li>
+          <li>Matemática: habilidad para utilizar números y hacer operaciones.</li>
+          <li>Científica: usar el método científico para adquirir conocimientos.</li>
+        </ol>
+
+        <h5>Habilidades</h5>
+        <ol> 
+          <li>Creatividad</li>
+          <li>Capacidad de organización</li>
+          <li>Resistencia</li>
+          <li>Manejo de recursos digitales</li>
+          <li>Manejo de redes sociales</li>
+          <li>Habilidad musical</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+
+      </div>
+    </div>
+  </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<br><br><br><br>
+<!--------------------------------------------------------->
+
+<div class="container">
+  <div id="portafolio">
+    <h1 class="display-1" align="center" style="background-color:#C1A5EE; color: white;";>Portafolio</h1>
+  <hr>
+    <div class="row">
+      <div class="col-6" align="center">
+        <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imag/img6.jpg" class="img-fluid rounded-start" alt="" >
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">1er. Cuatrimestre</h5>
+        <p class="card-text">
+        <ul> 
+          <li style="text-align: justify">Cell Street: En el primer cuatrimestre la empresa se llamaba Cell Street ya que el  proyecto consiste en una pantalla que ayuda a las personas con sus funciones a su vez hacia que nuestras calles sean más atractivas con su innovación por medio de un programa en Java</li>
+          </ul>  </p>
+      </div>
+    </div>
+  </div>
+</div> 
+      </div>
+      <div class="col-6" align="center">
+        <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imag/img7.jpg" class="img-fluid rounded-start" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">2do. Cuatrimestre</h5>
+        <p class="card-text"> <ul> 
+          <li style="text-align: justify">PUT IT IN RICE: En el segundo cuatrimestre la empresa se llamaba Put it in Rice ya que era una empresa dedicada a la venta y distribución de productos tecnológicos, contando con diferentes sucursales y en crecimiento. Ofrecia una gran cantidad de marcas en diferentes de dispositivos, como pueden ser computadoras, bocinas y celulares.</li>
+          </ul></p>
+       </div>
+    </div>
+  </div>
+</div> 
+      </div>
+    </div>
+  </div>
+</div>
+<!----------------------------------------->
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imag/img7.JPG" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Logotipo</h5>
+        <p>Logotido de la empresa Put it in Rice</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img8.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Mienbros de equipo</h5>
+        <p>Evidencia de los miembros del equipo</p></div>
+    </div>
+    <div class="carousel-item">
+      <img src="imag/img6.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+ 
+
+
+<br><br><br><br>
+<!---------------------------------------->
+<style type="text/css">
+  .form-contact{
+  width: 89%;
+  margin: 0 auto;
+  background-color: #C1A5EE;
+  background-size: cover;
+  background-position: center;
+  box-shadow: 20px 13px 37px rgba(0, 0, 0, .8);
+</style>
+<section class="form-contact" style="background;color: black;">
+<form action="" class="contact">
+<div id="intereces" class="row">
+  <div class="col-12" align="center">
+  <br>
+  <h1 class="display-1" style="background-color:#C1A5EE; color: white";>Intereses</h1>
+  <br>
+  </div>
+</div>
+<div class="row">
+  <div class="col-4" align="center">
+   <div class="card" style="width: 18rem;">
+    <div class="card-body">
+     <i class="fa-solid fa-tv" style="font-size:40px"></i><br><br><br><h5 class="card-title" style="background;color: black;">Documentales</h5>
+      <p class="card-text" style="text-align: justify">A mí me gusta ver los documentales  de la naturaleza o de animales salvajes por qué  son películas que relatan la vida de animales, plantas y otras criaturas vivas en su hábitat natural ya que en  algunas ocasiones, se muestran los ecosistemas de los seres vivos en relación al ser humano.</p>
+    </div>
+  </div> 
+</div>
+  <div class="col-4" align="center">
+    <div class="card" style="width: 18rem" align="center";>
+    <div class="card-body">
+      <i class="fa-solid fa-music" style="font-size:40px"></i>
+      <br><br><br><h5 class="card-title">Musica</h5>
+      <p class="card-text" style="text-align: justify">Me encanta escuchar diferentes tipos de música ya  que para cada momento de mi día a día tengo diferentes canciones para las distintas cosas que voy hacer  ya que cuando hago ejercicio escucho música que me motiva, cuando realizo mi tarea escucho  música que me relaja y me concentre y cuando hago deberes de mi casa pongo música que hace que me divierte.</p>
+    </div>
+  </div> 
+  </div>
+  <div class="col-4" align="center">
+   <div class="card" style="width: 18rem;">
+    
+    <div class="card-body">
+       <i class="fa-solid fa-book-open-reader" style="font-size:40px"></i>
+       <br><br><br>
+      <h5 class="card-title">Leer</h5>
+      <p class="card-text" style="text-align: justify">Uno de mis interés favoritos es leer libros  que se relacionen a la ficción, al arte   y libros relacionados al amor. Unas de más cosas del por qué me gusta leer es  que así aprendo palabras que no conozco o algunas veces te da información que no conocías</p>
+    </div>
+  </div>  
+  </div>
+</div>
+<br>
+</form>
+</section>
+
+<br><br><br><br>
+<!----------------------------------------------->
+<style type="text/css">
+  .form-contact{
+  width: 89%;
+  margin: 0 auto;
+  background-color:#C1A5EE; color: white;
+  background-size: cover;
+  background-position: center;
+  box-shadow: 20px 18px 37px rgba(0, 0, 0, .8);
+</style>
+<section class="form-contact">
+<div class="row" id="Contacto" align="center">
+  <div class="col-12 ">
+    <h1 class="display-1">Contacto</h1>
+
+    <div class="row">
+      <div class="col-6">
+        <br>
+        <br>
+        <br>
+        <h2>Redes sociales</h2>
+        <br>
+        <a href="https://www.facebook.com/marcela.azuararivera.1/"><i class="fa-brands fa-facebook-square" style="font-size:50px"></i></a>AR Marcela
+        <br>
+        <br>
+        
+        <a href="https://www.instagram.com/march_ar11/"><i class="fa-brands fa-instagram" style="font-size:50px"></i></a> march_ar11
+        <br>
+       
+      </div>
+
+      <div class="col-6">
+        <i class="fa-solid fa-location-dot" style="font-size:50px"></i><h4>Casa</h4>
+        <br><br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.969463987816!2d-98.26398388255615!3d19.1528139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc4fc5a468ca7%3A0xcb1692ae0aae7edd!2sCastellon%2032%2C%20Misiones%20de%20San%20Francisco%2C%20Pue.!5e0!3m2!1ses-419!2smx!4v1657684782094!5m2!1ses-419!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <br><br>
+</div>
+</div>
+</div>
+
+<br><br><br>
+<div class="row">
+  <center> <h2>Contactos</h2>
+        <div class="col-6">
+         
+        <i class="fa-solid fa-phone" style="font-size:48px"></i><p>Telefono 2211032754</p>
+        <br><br>
+      </div>
+        <div class="col-6">
+        <i class="fa-solid fa-envelope" style="font-size:48px"></i><p>3521110176@uth.edu.mx
+        o marcelazuarariver@gmail.com</p>
+        <br><br>
+      </div>
+    </div>
+</center>
+</div>
+</form>
+</section>
+
+<br><br><br><br>
+<div class="row" align="center" style="background-color:#C1A5EE; color: white;" >
+  <div class="col" >
+    <footer>
+      <p>
+        Derechos reservados 2022 
+      </p>
+    </footer>
+  </div>
+</div>
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
